a3a9a5cad72aae831d6dbacae8eebd5ecb9f2d1f
AssisrMatheus <assisr.matheus@gmail.com>
2022-08-29 16:13:59 -0400

    Rollbacks buildBlockSchema method

--- library/src/block.ts
+++ library/src/block.ts
@@ -1,5 +1,6 @@
 import { Control, ControlSchema, MapControlTypes } from "./control";
 
+// This should return the title of ALL blocks
 export type ControlDeclaration<C extends Control = Control> =
   C extends ControlSchema<infer ControlName> ? { name: ControlName } : never;
 
@@ -7,11 +8,8 @@ export type BlockControl = {
   [key: string]: ControlDeclaration;
 };
 
-export type BlockSchema<
-  BlockTitle extends string = string,
-  BC extends BlockControl = BlockControl
-> = {
-  title: BlockTitle;
+export type BlockSchema<BC extends BlockControl = BlockControl> = {
+  title: string;
   controls: BC;
 };
 
@@ -20,22 +18,15 @@ export type AllBlocksType<T extends BlockSchema = BlockSchema> = T;
 
 export type Block = AllBlocksType | AllBlocksInterface;
 
-// This should return the title of ALL blocks
-export type BlockTitles = Block["title"];
-
 // Ref: https://stackoverflow.com/questions/71785211/how-to-infer-types-of-an-object-created-from-a-schema
-export type InferBlockPropsFromControl<BS> = BS extends BlockSchema<
-  infer _BlockTitle,
-  infer BC
->
+export type InferBlockPropsFromControl<BS> = BS extends BlockSchema<infer BC>
   ? { [P in keyof BC]: MapControlTypes<BC[P]["name"]> }
   : never;
 
 // Ref: https://stackoverflow.com/questions/71785211/how-to-infer-types-of-an-object-created-from-a-schema
 export const buildBlockSchema = <
-  BlockTitle extends string,
-  BC extends BlockControl = BlockControl,
-  B extends BlockSchema<BlockTitle, BC> = BlockSchema<BlockTitle, BC>
+  BC extends BlockControl,
+  B extends BlockSchema<BC> = BlockSchema<BC>
 >(
   b: B
 ) => b;
